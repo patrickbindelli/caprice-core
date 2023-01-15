9e79aaa71576d88c3ed37ae8fcd58d89e8344efa
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-01-14 23:24:19 -0300

    Adding base project

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) => [
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`,
+  `yarn prettier --write ${filenames.join(" ")}`,
+];
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "**/*.(md|json)": (filenames) =>
+    `yarn prettier --write ${filenames.join(" ")}`,
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Caprice</title>
+      </Head>
+
+      <main>
+        <h1>Hello, world!</h1>
+      </main>
+    </>
+  );
+}
