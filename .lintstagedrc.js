595732241cd4fbe073ceef1825c1e510dc28c9ba
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-01-14 23:39:15 -0300

    Changing project default config to use single quotes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
-const path = require("path");
+const path = require('path');
 
 const buildEslintCommand = (filenames) => [
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`,
-  `yarn prettier --write ${filenames.join(" ")}`,
+    .join(' --file ')}`,
+  `yarn prettier --write ${filenames.join(' ')}`,
 ];
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "**/*.(md|json)": (filenames) =>
-    `yarn prettier --write ${filenames.join(" ")}`,
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
 };
