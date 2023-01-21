cd24ffbc3b01eec9a8d50deb1a5b05ac22ed68bd
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-01-21 19:09:50 -0300

    feat: improving navbar styles

--- src/components/Navbar/Navbar.styles.tsx
+++ src/components/Navbar/Navbar.styles.tsx
@@ -14,16 +14,38 @@ export const Container = styled.div.attrs(({ width }: NavbarProps) => ({
   position: relative;
   min-width: 200px;
   max-width: 500px;
-  padding: 2px 10px;
   transition: width 300ms ease-out;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Header = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  padding: 10px;
+  overflow-y: auto;
+`;
+
+export const Footer = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.colors.primary.main}70;
+  padding: 10px;
 `;
 
 export const Title = styled.div`
-  color: ${({ theme }) => theme.colors.text.primary};
+  flex: 1 1 auto;
   display: flex;
-  justify-content: flex-start;
   align-items: center;
+  color: ${({ theme }) => theme.colors.text.primary};
   font-size: 1.5em;
-  height: 50px;
-  padding: 0px 10px;
 `;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import { Container, Title } from './Navbar.styles';
+import { GrAdd, GrDocumentText, GrPerformance, GrSearch } from 'react-icons/gr';
 import { NavbarButton } from '../NavbarButton';
-import { GrDocumentText, GrSearch } from 'react-icons/gr';
+import { Container, Content, Footer, Header, Title } from './Navbar.styles';
 interface Props {
   width: number;
 }
+
+const documents = [
+  {
+    title: 'Conceitos Básicos em Gerência de Projetos',
+  },
+];
 
 const Navbar: React.FC<Props> = ({ width }) => {
   const ICON_SIZE = 20;
   return (
     <Container width={width}>
-      <Title>Caprice</Title>
-      <NavbarButton>
-        <GrSearch size={ICON_SIZE} />
-        Pesquisar
-      </NavbarButton>
-      <NavbarButton>
-        <GrDocumentText size={ICON_SIZE} />
-        Novo Documento
-      </NavbarButton>
+      <Header>
+        <Title>Caprice</Title>
+      </Header>
+      <Content>
+        {documents.map((document, index) => {
+          return (
+            <NavbarButton
+              key={index}
+              icon={<GrDocumentText size={ICON_SIZE} />}
+            >
+              {document.title}
+            </NavbarButton>
+          );
+        })}
+      </Content>
+      <Footer>
+        <NavbarButton icon={<GrAdd size={ICON_SIZE} />}>
+          Nova Pagina
+        </NavbarButton>
+        <NavbarButton icon={<GrSearch size={ICON_SIZE} />}>
+          Pesquisar
+        </NavbarButton>
+        <NavbarButton icon={<GrPerformance size={ICON_SIZE} />}>
+          Configurações
+        </NavbarButton>
+      </Footer>
     </Container>
   );
 };
